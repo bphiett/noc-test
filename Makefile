9670ec9354f56cf817913f6fcd9386b0614f4a39
Ben Hiett <ben@hiett.co.uk>
2020-06-16 22:59:30 +0100

    q7.c added

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: q2 q4 q15
+all: q2 q4 q7 q15
 
 q2: q2.c
 	gcc -g -o q2 q2.c
 
 q4: q4.c
 	gcc -g -o q4 q4.c
+
+q7: q7.c
+	gcc -g -o q7 q7.c
 
 q15: q15.c
 	gcc -g -o q15 q15.c
---
+++ q7.c
@@ -0,0 +1,16 @@
+/**
+ * NOC C Test
+ *
+ * Question 7
+ *
+ * Classic Hello World!
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");
+
+    return 0;
+}

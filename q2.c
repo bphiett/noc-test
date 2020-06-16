d0bac849d3fff2f315fb88a59233a0828df1cf19
Ben Hiett <ben@hiett.co.uk>
2020-06-16 22:40:51 +0100

    q2 added

---
+++ q2.c
@@ -0,0 +1,21 @@
+/**
+ * NOC C Test
+ *
+ * Question 2
+ *
+ * Using math.h library
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    double x;
+
+    x = ceil(1.66);
+
+    printf("ceil(1.66) = %f\n", x);
+    
+    return 0;
+}
